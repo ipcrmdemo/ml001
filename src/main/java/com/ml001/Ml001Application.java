060d7ad4ecc0d9fc91fb03d093e758d5cadd9435
Matt Cadorette <mcadorette@gmail.com>
2018-12-11 09:15:17 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/ml001/Ml001Application.java
@@ -0,0 +1,12 @@
+package com.ml001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ml001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ml001Application.class, args);
+	}
+}
